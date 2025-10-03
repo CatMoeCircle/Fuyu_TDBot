3b63db808da690d321fb5c08b55aa9297614104b
xiaoqvan <104122098+xiaoqvan@users.noreply.github.com>
2025-10-03 18:38:58 +0800

    fix: start命令为测试文本的问题

--- src/cmd/start.ts
+++ src/cmd/start.ts
@@ -19,23 +19,8 @@ export default class start extends Plugin {
         handler: async (updateNewMessage, _args) => {
           try {
             // 发送消息示例（使用 client.invoke）
-            const text = await client.invoke({
-              _: "parseTextEntities",
-              text: `*粗体 \\*文本*\n_斜体 \\*文本_\n__下划线__\n~删除线~\n||剧透||\n*粗体 _斜体粗体 ~斜体粗体删除线 ||斜体粗体删除线剧透||~ __下划线斜体粗体___ 粗体*\n[内联网址](http://www.example.com/)\n[内联提及用户](tg://user?id=5895998976)\n![👍](tg://emoji?id=5368324170671202286)\n\`内联等宽代码\`\n\`\`\`预格式化的固定宽度代码块\`\`\`\n\`\`\`python\n用 Python 编程语言编写的预格式化固定宽度代码块\n\`\`\`\n>块引用开始\n>块引用继续\n>块引用继续\n>块引用继续\n>块引用的最后一行\n**>可展开的块引用紧接在前一个块引用之后开始\n>它与前一个块引用之间由一个空的粗体实体分隔\n>可展开的块引用继续\n>可展开的块引用默认隐藏的部分开始\n>可展开的块引用继续\n>可展开的块引用的最后一行带有可展开性标记||`,
-              parse_mode: {
-                _: "textParseModeMarkdown",
-                version: 2,
-              },
-            });
-            console.log(text);
-
             await sendMessage(this.client, updateNewMessage.message.chat_id, {
-              invoke: {
-                input_message_content: {
-                  _: "inputMessageText",
-                  text: text,
-                },
-              },
+              text: "Hello! 欢迎使用本 Bot。\n\n使用 /help 查看可用命令。\n\n本bot由 [Fuyu_TDBot](https://github.com/CatMoeCircle/Fuyu_TDBot) 框架驱动",
             });
           } catch (e) {
             logger.error("发送消息失败", e);
