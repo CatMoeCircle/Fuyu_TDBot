8dc8ddd747bfc340e1c038f9f9eddad53ee9893e
xiaoqvan <104122098+xiaoqvan@users.noreply.github.com>
2025-10-13 22:10:06 +0800

    add 添加字体支持

--- src/function/gen_png.ts
+++ src/function/gen_png.ts
@@ -2,26 +2,52 @@ import { defineSatoriConfig, satoriVue } from "x-satori/vue";
 import { Resvg } from "@resvg/resvg-js";
 import fs from "fs";
 import path from "path";
+import NotoSansSC from "../fonts/NotoSansSC-Regular.ttf?file";
 
-const fontPath = path.join("C:\\Windows\\Fonts", "simhei.ttf");
+// 直接使用路径引用字体文件
+
+/* 字体字体的粗细程度 */
+type Weight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+/* 字体的风格 */
+type FontStyle = "normal" | "italic";
+/* 字体选项 */
+interface FontOptions {
+  /* 字体文件的数据，可以是 Buffer 或 ArrayBuffer */
+  data: Buffer | ArrayBuffer;
+  /* 字体的名称 */
+  name: string;
+  /* 字体的粗细程度，默认为 400 */
+  weight?: Weight;
+  /* 字体的风格，默认为 "normal" */
+  style?: FontStyle;
+  /* 可选的语言标签 */
+  lang?: string;
+}
 
 export async function generatePng(
+  options: {
+    width: number;
+    height: number;
+    fonts?: FontOptions[];
+  },
   vuetemplateStr: string,
   props?: Record<string, any>
 ): Promise<string> {
   const opt = defineSatoriConfig({
-    width: 800,
-    height: 600,
+    width: options.width || 800,
+    height: options.height || 600,
     fonts: [
       {
-        name: "Arial",
-        data: await fs.promises.readFile(fontPath),
+        name: "Noto Sans SC",
+        data: await fs.promises.readFile(NotoSansSC),
         weight: 400,
         style: "normal",
       },
+      ...(options.fonts || []),
     ],
     props,
   });
+
   const strSVG = await satoriVue(opt, vuetemplateStr);
 
   const resvg = new Resvg(strSVG, {
